44c38b02ffd355d3abc4ab03d8d5eba48b6831fa
jin Bal <jinbal@hotmail.com>
2015-06-01 23:25:24 +0100

    s

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,45 +1,43 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import akka.actor.{Actor, ActorRef, Props}
 import kvstore.Arbiter._
-import scala.collection.immutable.Queue
-import akka.actor.SupervisorStrategy.Restart
-import scala.annotation.tailrec
-import akka.pattern.{ ask, pipe }
-import akka.actor.Terminated
-import scala.concurrent.duration._
-import akka.actor.PoisonPill
-import akka.actor.OneForOneStrategy
-import akka.actor.SupervisorStrategy
-import akka.util.Timeout
+import kvstore.Replica._
 
 object Replica {
+
   sealed trait Operation {
     def key: String
+
     def id: Long
   }
+
   case class Insert(key: String, value: String, id: Long) extends Operation
+
   case class Remove(key: String, id: Long) extends Operation
+
   case class Get(key: String, id: Long) extends Operation
 
   sealed trait OperationReply
+
   case class OperationAck(id: Long) extends OperationReply
+
   case class OperationFailed(id: Long) extends OperationReply
+
   case class GetResult(key: String, valueOption: Option[String], id: Long) extends OperationReply
 
   def props(arbiter: ActorRef, persistenceProps: Props): Props = Props(new Replica(arbiter, persistenceProps))
 }
 
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
-  import Replica._
-  import Replicator._
-  import Persistence._
-  import context.dispatcher
 
   /*
    * The contents of this actor is just a suggestion, you can implement it in any way you like.
    */
-  
+
+  arbiter ! Join
+
+
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
@@ -48,12 +46,18 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
 
 
   def receive = {
-    case JoinedPrimary   => context.become(leader)
+    case JoinedPrimary => context.become(leader)
     case JoinedSecondary => context.become(replica)
   }
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
+    case Insert(key, value, id) =>
+      insert(key, value, id)
+    case Remove(key, id) =>
+      remove(key, id)
+    case Get(key, id) =>
+      find(key, id)
     case _ =>
   }
 
@@ -61,6 +65,21 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   val replica: Receive = {
     case _ =>
   }
+
+  def find(key: String, id: Long): Unit = {
+    sender ! GetResult(key, kv.get(key), id)
+  }
+
+  def remove(key: String, id: Long): Unit = {
+    kv -= (key)
+    sender ! OperationAck(id)
+  }
+
+  def insert(key: String, value: String, id: Long): Unit = {
+    kv += (key -> value)
+    sender ! OperationAck(id)
+  }
+
 
 }
 
